3e0c56fae0b1f621549056249a17334eb456f4d6
Juan González <juan.gonzalez@xing.com>
2018-12-02 23:56:49 +0100

    Problem 2, part 1 - I don't like this solution

--- problem_2/lib/problem_2.ex
+++ problem_2/lib/problem_2.ex
@@ -1,4 +1,29 @@
 defmodule Problem2 do
+  def checksum(file_path) do
+    file_path |> read_lines() |> calculate_checksum([2,3])
+  end
+
+  def calculate_checksum(word_list, checksum_frequencies) do
+    word_list
+    |> process_words()
+    |> Enum.flat_map(fn {_word, char_frequencies} ->
+      char_frequencies
+      |> Enum.filter(fn {_char, frequency} -> checksum_frequencies |> Enum.member?(frequency) end)
+      |> Enum.uniq_by(fn {_char, frequency} -> frequency end)
+    end)
+    |> Enum.reduce(%{}, fn {_char, frequency}, acc ->
+      acc |> Map.put(frequency, Map.get(acc, frequency, 0) + 1)
+    end)
+    |> Enum.reduce(1, fn {_, count}, acc -> acc * count end)
+  end
+
+  def process_words(word_list) do
+    word_list
+    |> Enum.reduce(%{}, fn word, acc ->
+      acc |> Map.put(word, count_letters(word))
+    end)
+  end
+
   def count_letters(word) do
     word
     |> String.graphemes()
@@ -6,5 +31,15 @@ defmodule Problem2 do
       count = (acc |> Map.get(char, 0)) + 1
       acc |> Map.put(char, count)
     end)
+  end
+
+  # TODO Same is in problem1, extract this
+  defp read_lines(file_path) do
+    with {:ok, file_content} <- file_path |> File.read(),
+         lines <- file_content |> String.split() do
+      lines
+    else
+      res -> res
+    end
   end
 end
--- problem_2/test/problem_2_test.exs
+++ problem_2/test/problem_2_test.exs
@@ -20,4 +20,44 @@ defmodule Problem2Test do
       assert count_letters(input) == expected
     end
   end
+
+  describe "process_words/1" do
+    import Problem2, only: [process_words: 1]
+
+    test "calculates and returns all the letter frequencies" do
+      input = "abcabcdera"
+
+      expected = %{
+        input => %{
+          "a" => 3,
+          "b" => 2,
+          "c" => 2,
+          "d" => 1,
+          "e" => 1,
+          "r" => 1
+        }
+      }
+
+      assert process_words([input]) == expected
+    end
+  end
+
+  describe "calculate_checksum/2" do
+    import Problem2, only: [calculate_checksum: 2]
+
+    test "calculates the checksum" do
+      words = ~w{abcdef bababc abbcde abcccd aabcdd abcdee ababab}
+      expected_checksum = 12
+
+      assert calculate_checksum(words, [2,3]) == expected_checksum
+    end
+  end
+
+  describe "checksum/1" do
+    import Problem2, only: [checksum: 1]
+
+    test "calculates the checksum" do
+      assert checksum("input.txt") == 6696
+    end
+  end
 end
