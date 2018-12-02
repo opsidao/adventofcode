5273e9af57b8d88992bdb32f55f18e388d02f545
Juan González <juan.gonzalez@xing.com>
2018-12-02 18:33:48 +0100

    Make it recursive and circular for the second part

--- problem_1/lib/problem_1.ex
+++ problem_1/lib/problem_1.ex
@@ -14,6 +14,24 @@ defmodule Problem1 do
   end
 
   def calculate_frequency(input_frequencies) do
-    input_frequencies |> Enum.map(&(String.to_integer(&1))) |> Enum.sum()
+    input_frequencies = input_frequencies |> Enum.map(&(String.to_integer(&1)))
+
+    rec_calculate_frequency(input_frequencies, input_frequencies, MapSet.new([0]), 0)
+  end
+
+  defp rec_calculate_frequency(initial_frequencies, [current_frequency|other_frequencies], previous_frequencies, last_frequency) do
+    new_frequency = last_frequency + current_frequency
+
+    if previous_frequencies |> MapSet.member?(new_frequency) do
+      new_frequency
+    else
+      previous_frequencies = previous_frequencies |> MapSet.put(new_frequency)
+
+      if length(other_frequencies) == 0 do
+        rec_calculate_frequency(initial_frequencies, initial_frequencies, previous_frequencies, new_frequency)
+      else
+        rec_calculate_frequency(initial_frequencies, other_frequencies, previous_frequencies, new_frequency)
+      end
+    end
   end
 end
--- problem_1/test/problem_1_test.exs
+++ problem_1/test/problem_1_test.exs
@@ -9,19 +9,35 @@ defmodule Problem1Test do
       lines = read_lines("input.txt")
 
       assert is_list(lines)
-      assert tuple_size(List.to_tuple lines) == 959
+      assert tuple_size(List.to_tuple(lines)) == 959
     end
   end
 
   describe "calculate_frequency/1" do
     import Problem1, only: [calculate_frequency: 1]
 
-    test "it calculates the frequency" do
-      input = ["+1", "+2", "-3", "+10", "-17"]
-
+    test "finds the first repeated frequency when it's 0" do
+      input = ["+1", "-1"]
       frequency = calculate_frequency(input)
+      assert frequency === 0
+    end
 
-      assert frequency === -7
+    test "finds the first repeated frequency when it's 10" do
+      input = ["+3", "+3", "+4", "-2", "-4"]
+      frequency = calculate_frequency(input)
+      assert frequency === 10
+    end
+
+    test "finds the first repeated frequency when it's 5" do
+      input = ["-6", "+3", "+8", "+5", "-6"]
+      frequency = calculate_frequency(input)
+      assert frequency === 5
+    end
+
+    test "finds the first repeated frequency when it's 14" do
+      input = ["+7", "+7", "-2", "-7", "-4"]
+      frequency = calculate_frequency(input)
+      assert frequency === 14
     end
   end
 
@@ -31,7 +47,7 @@ defmodule Problem1Test do
     test "it reads the file and calculates the frequency" do
       frequency = process_file("input.txt")
 
-      assert frequency === 408
+      assert frequency === 55250
     end
   end
 end
