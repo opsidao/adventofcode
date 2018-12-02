aba9e94d145861cf4f0688e82ea52df475bb97aa
Juan González <juan.gonzalez@xing.com>
2018-12-02 17:25:43 +0100

    mix new problem_1

---
+++ problem_1/lib/problem_1.ex
@@ -0,0 +1,18 @@
+defmodule Problem1 do
+  @moduledoc """
+  Documentation for Problem1.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Problem1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ problem_1/test/problem_1_test.exs
@@ -0,0 +1,8 @@
+defmodule Problem1Test do
+  use ExUnit.Case
+  doctest Problem1
+
+  test "greets the world" do
+    assert Problem1.hello() == :world
+  end
+end
