da481f7c281374a104d80d62a26a074cfb50cdd5
Juan González <juan.gonzalez@xing.com>
2018-12-02 18:43:43 +0100

    mix new problem_2

---
+++ problem_2/lib/problem_2.ex
@@ -0,0 +1,18 @@
+defmodule Problem2 do
+  @moduledoc """
+  Documentation for Problem2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Problem2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
