a9bfd861479e3d0094de17f1766b0f78980e5978
Juan González <juan.gonzalez@xing.com>
2018-12-03 12:09:47 +0100

    Initial project for problem 3

---
+++ problem_3/lib/problem_3.ex
@@ -0,0 +1,18 @@
+defmodule Problem3 do
+  @moduledoc """
+  Documentation for Problem3.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Problem3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
