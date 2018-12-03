9f064da8310f2b71a49ffca9a7c4b5e8ee907111
Juan González <juan.gonzalez@xing.com>
2018-12-03 19:09:32 +0100

    Day 3, part 2

--- problem_3/lib/problem_3.ex
+++ problem_3/lib/problem_3.ex
@@ -4,17 +4,7 @@ defmodule Problem3 do
       file_path
       |> File.stream!([], :line)
       |> Enum.reduce(%{}, fn claim, acc ->
-        [_claim_id, "@", coords, size] = claim |> String.split()
-        [width, height] = size |> String.split("x") |> Enum.map(&String.to_integer/1)
-
-        [x, y] =
-          coords
-          |> String.replace_trailing(":", "")
-          |> String.split(",")
-          |> Enum.map(&String.to_integer/1)
-
-        x_range = x..(x + width - 1)
-        y_range = y..(y + height - 1)
+        [claim_id, x_range, y_range] = extract_claim_details(claim)
 
         x_range
         |> Enum.reduce(acc, fn x, acc ->
@@ -34,5 +24,59 @@ defmodule Problem3 do
       overlaps = column |> Map.values() |> Enum.filter(&(&1 > 1)) |> length()
       acc + overlaps
     end)
+  end
+
+  def find_non_overlapping_claim(file_path) when is_bitstring(file_path) do
+    file_path
+    |> File.stream!([], :line)
+    |> Enum.reduce(%{}, fn claim, acc ->
+      [claim_id, x_range, y_range] = extract_claim_details(claim)
+
+      x_range
+      |> Enum.reduce(acc, fn x, acc ->
+        y_range
+        |> Enum.reduce(acc, fn y, acc ->
+          column = acc |> Map.get(x, %{})
+          {count, claims} = column |> Map.get(y, {0, []})
+          column = column |> Map.put(y, {count + 1, [claim_id, claims]})
+
+          acc |> Map.put(x, column)
+        end)
+      end)
+    end)
+    |> find_non_overlapping_claim()
+  end
+
+  def find_non_overlapping_claim(tiles_map) when is_map(tiles_map) do
+    claims = tiles_map
+    |> Map.values()
+    |> Enum.flat_map(& &1)
+    |> Enum.map(fn {_, {_, claims}} -> claims end)
+
+    claims_with_overlap = claims
+    |> Enum.filter(fn [_head,tail] -> length(tail) > 0 end)
+    |> List.flatten()
+    |> Enum.uniq()
+
+    all_claims = claims
+    |> List.flatten()
+    |> Enum.uniq()
+
+    [claim_id] = all_claims -- claims_with_overlap
+
+    claim_id |> String.replace("#", "") |> String.to_integer
+  end
+
+  defp extract_claim_details(claim) do
+    [claim_id, "@", coords, size] = claim |> String.split()
+    [width, height] = size |> String.split("x") |> Enum.map(&String.to_integer/1)
+
+    [x, y] =
+      coords
+      |> String.replace_trailing(":", "")
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+
+    [claim_id, x..(x + width - 1), y..(y + height - 1)]
   end
 end
--- problem_3/test/problem_3_test.exs
+++ problem_3/test/problem_3_test.exs
@@ -2,13 +2,27 @@ defmodule Problem3Test do
   use ExUnit.Case
   doctest Problem3
 
-  import Problem3, only: [calculate_overlaps: 1]
+  describe "calculate_overlaps/1" do
+    import Problem3, only: [calculate_overlaps: 1]
 
-  test "calculates the overlapping area" do
-    assert calculate_overlaps("test_input.txt") == 4
+    test "calculates the overlapping area" do
+      assert calculate_overlaps("test_input.txt") == 4
+    end
+
+    test "calculates the overlapping area on the real file" do
+      assert calculate_overlaps("input.txt") == 111_630
+    end
   end
 
-  test "calculates the overlapping area on the real file" do
-    assert calculate_overlaps("input.txt") == 111_630
+  describe "find_non_overlapping_claim/1" do
+    import Problem3, only: [find_non_overlapping_claim: 1]
+
+    test "returns the claim with no overlaps" do
+      assert find_non_overlapping_claim("test_input.txt") == 3
+    end
+
+    test "returns the claim with no overlaps on the real file" do
+      assert find_non_overlapping_claim("input.txt") == 724
+    end
   end
 end
