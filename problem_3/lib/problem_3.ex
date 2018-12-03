e09de0be6bc98a3d0c4a56809e971766d136d6d6
Juan González <juan.gonzalez@xing.com>
2018-12-03 13:49:53 +0100

    Some dirty stuff, but problem_3 part_1 done

--- problem_3/lib/problem_3.ex
+++ problem_3/lib/problem_3.ex
@@ -1,18 +1,38 @@
 defmodule Problem3 do
-  @moduledoc """
-  Documentation for Problem3.
-  """
+  def calculate_overlaps(file_path) do
+    used_claims =
+      file_path
+      |> File.stream!([], :line)
+      |> Enum.reduce(%{}, fn claim, acc ->
+        [_claim_id, "@", coords, size] = claim |> String.split()
+        [width, height] = size |> String.split("x") |> Enum.map(&String.to_integer/1)
 
-  @doc """
-  Hello world.
+        [x, y] =
+          coords
+          |> String.replace_trailing(":", "")
+          |> String.split(",")
+          |> Enum.map(&String.to_integer/1)
 
-  ## Examples
+        x_range = x..(x + width - 1)
+        y_range = y..(y + height - 1)
 
-      iex> Problem3.hello()
-      :world
+        x_range
+        |> Enum.reduce(acc, fn x, acc ->
+          y_range
+          |> Enum.reduce(acc, fn y, acc ->
+            column = acc |> Map.get(x, %{})
+            count = column |> Map.get(y, 0)
+            column = column |> Map.put(y, count + 1)
 
-  """
-  def hello do
-    :world
+            acc |> Map.put(x, column)
+          end)
+        end)
+      end)
+
+    used_claims
+    |> Enum.reduce(0, fn {_column_number, column}, acc ->
+      overlaps = column |> Map.values() |> Enum.filter(&(&1 > 1)) |> length()
+      acc + overlaps
+    end)
   end
 end
--- problem_3/test/problem_3_test.exs
+++ problem_3/test/problem_3_test.exs
@@ -2,7 +2,13 @@ defmodule Problem3Test do
   use ExUnit.Case
   doctest Problem3
 
-  test "greets the world" do
-    assert Problem3.hello() == :world
+  import Problem3, only: [calculate_overlaps: 1]
+
+  test "calculates the overlapping area" do
+    assert calculate_overlaps("test_input.txt") == 4
+  end
+
+  test "calculates the overlapping area on the real file" do
+    assert calculate_overlaps("input.txt") == 111_630
   end
 end
